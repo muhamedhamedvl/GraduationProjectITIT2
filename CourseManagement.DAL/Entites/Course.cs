f4cbefff151470025c92009cb1c77f9af080d903
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-30 19:34:29 +0300

    Update Course.cs

--- CourseManagement.DAL/Entites/Course.cs
+++ CourseManagement.DAL/Entites/Course.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Collections.Specialized.BitVector32;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace CourseManagement.DAL.Entites
 {
-
     public class Course
     {
-       
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        //[NoNumber]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; }
+
+        public string? Description { get; set; }
+
+        // FK
+        [Required]
+        public int InstructorId { get; set; }
+
+        [ForeignKey("InstructorId")]
+        public Instructor Instructor { get; set; }
+    
     }
 }
