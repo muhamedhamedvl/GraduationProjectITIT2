54ae1c3000b0506bd8b3d082b70ec1f4e324e08c
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-30 21:08:28 +0300

    Update AppDbContext.cs

--- CourseManagement.DAL/Data/AppDbContext.cs
+++ CourseManagement.DAL/Data/AppDbContext.cs
@@ -14,6 +14,8 @@ namespace CourseManagement.DAL.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Course> Courses { get; set; }
+                public DbSet<Instructor> Instructors { get; set; }
+
         public DbSet<Session> Sessions { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Grade> Grades { get; set; }
