6abf45f3c16132ee321dd3de36913b6f5d3c8e03
HagarMahmoud128 <hagar44mahmoud@gmail.com>
2025-08-30 21:56:50 +0300

    ISessionService.cs

--- CourseManagement.BLL/Services/Interfaces/ISessionService.cs
+++ CourseManagement.BLL/Services/Interfaces/ISessionService.cs
@@ -1,14 +1,16 @@
-﻿using CourseManagement.BLL.ViewModels;
-using System;
+using CourseManagement.BLL.ViewModels;
+using CourseManagement.DAL.Entites;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CourseManagement.BLL.Services.Interfaces
 {
     public interface ISessionService
     {
-
+        Task<IEnumerable<Session>> GetAllSessionsAsync(string courseName = null, int page = 1, int pageSize = 10);
+        Task<Session> GetSessionByIdAsync(int id);
+        Task AddSessionAsync(SessionViewModel model);
+        Task UpdateSessionAsync(int id, SessionViewModel model);
+        Task DeleteSessionAsync(int id);
     }
 }
