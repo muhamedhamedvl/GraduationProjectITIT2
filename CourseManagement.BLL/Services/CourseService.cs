33a78101ae7fdf11f40e51d478cf42eae0af8b54
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-30 19:35:12 +0300

    Update CourseService.cs

--- CourseManagement.BLL/Services/CourseService.cs
+++ CourseManagement.BLL/Services/CourseService.cs
@@ -1,7 +1,6 @@
-﻿using CourseManagement.BLL.Services.Interfaces;
-using CourseManagement.BLL.ViewModels;
-using CourseManagement.DAL.Entites;
-using CourseManagement.DAL.Repositores.Interfaces;
+using CourseManagement.BLL.Interfaces;
+using CourseManagement.DAL.Interfaces;
+using CourseManagement.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +11,35 @@ namespace CourseManagement.BLL.Services
 {
     public class CourseService : ICourseService
     {
+        private readonly ICourseRepository _repo;
+        public CourseService(ICourseRepository repo)
+        {
+            _repo = repo;
+        }
 
+        public Task<IEnumerable<Course>> GetPagedCoursesAsync(string? search, string? category, int page, int pageSize)
+            => _repo.GetAllAsync(search, category, page, pageSize);
+
+        public Task<int> GetCoursesCountAsync(string? search, string? category)
+            => _repo.CountAsync(search, category);
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await _repo.GetAllCoursesAsync();
+        }
+        public Task<Course?> GetCourseAsync(int id)
+            => _repo.GetByIdAsync(id);
+
+        public Task AddCourseAsync(Course course)
+            => _repo.AddAsync(course);
+
+        public Task UpdateCourseAsync(Course course)
+            => _repo.UpdateAsync(course);
+
+        public Task DeleteCourseAsync(int id)
+            => _repo.DeleteAsync(id);
+
+        public Task<bool> IsCourseNameUniqueAsync(string name, int? excludeId = null)
+            => _repo.IsNameUniqueAsync(name, excludeId);
     }
+
 }
