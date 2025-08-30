4305862ad500ccb9a588b512adc8d6d4c484d3fd
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-30 19:35:40 +0300

    Update ICourseService.cs

--- CourseManagement.BLL/Services/Interfaces/ICourseService.cs
+++ CourseManagement.BLL/Services/Interfaces/ICourseService.cs
@@ -1,14 +1,24 @@
-﻿using CourseManagement.BLL.ViewModels;
+
+﻿using CourseManagement.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CourseManagement.BLL.Services.Interfaces
+namespace CourseManagement.BLL.Interfaces
 {
     public interface ICourseService
     {
-        
+        Task<IEnumerable<Course>> GetPagedCoursesAsync(string? search, string? category, int page, int pageSize);
+        Task<int> GetCoursesCountAsync(string? search, string? category);
+        Task<Course?> GetCourseAsync(int id);
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+        Task AddCourseAsync(Course course);
+        Task UpdateCourseAsync(Course course);
+        Task DeleteCourseAsync(int id);
+        Task<bool> IsCourseNameUniqueAsync(string name, int? excludeId = null);
+    
+
     }
 }
