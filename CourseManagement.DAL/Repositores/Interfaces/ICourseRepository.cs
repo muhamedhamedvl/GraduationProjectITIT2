b2bae930bb81817bfb0657310353c13920a9f0cb
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-30 19:36:34 +0300

    Update ICourseRepository.cs

--- CourseManagement.DAL/Repositores/Interfaces/ICourseRepository.cs
+++ CourseManagement.DAL/Repositores/Interfaces/ICourseRepository.cs
@@ -1,14 +1,21 @@
-﻿using CourseManagement.DAL.Entites;
+using CourseManagement.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CourseManagement.DAL.Repositores.Interfaces
+namespace CourseManagement.DAL.Interfaces
 {
-    public interface ICourseRepository : IGenericRepository<Course>
+    public interface ICourseRepository
     {
-       
+        Task<IEnumerable<Course>> GetAllAsync(string? search, string? category, int page, int pageSize);
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+        Task<int> CountAsync(string? search, string? category);
+        Task<Course?> GetByIdAsync(int id);
+        Task AddAsync(Course course);
+        Task UpdateAsync(Course course);
+        Task DeleteAsync(int id);
+        Task<bool> IsNameUniqueAsync(string name, int? excludeId = null);
     }
 }
