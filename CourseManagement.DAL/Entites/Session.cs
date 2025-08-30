64909d869dc1da0d4ed951656a1cb7d0e77d193e
HagarMahmoud128 <hagar44mahmoud@gmail.com>
2025-08-30 21:59:48 +0300

    Session.cs

--- CourseManagement.DAL/Entites/Session.cs
+++ CourseManagement.DAL/Entites/Session.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CourseManagement.DAL.Entites;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseManagement.DAL.Entites
 {
     public class Session
     {
-       
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
     }
 }
